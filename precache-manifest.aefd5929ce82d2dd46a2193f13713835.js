c0712a0e1f7ad5257d0b326595a9401d3206807e
ParkashSuther <41523939+ParkashSuther@users.noreply.github.com>
2020-10-14 22:10:01 +0500

    Add files via upload

---
+++ precache-manifest.aefd5929ce82d2dd46a2193f13713835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c0a49af1d986731616d790798ad992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62df60dac759ce4e7c03",
+    "url": "/static/css/main.0a744463.chunk.css"
+  },
+  {
+    "revision": "29ea04c457e643d25997",
+    "url": "/static/js/2.d0c4baf6.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.d0c4baf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62df60dac759ce4e7c03",
+    "url": "/static/js/main.eea2ec1d.chunk.js"
+  },
+  {
+    "revision": "ab22012da8b97503f869",
+    "url": "/static/js/runtime-main.8e832b81.js"
+  }
+]);
